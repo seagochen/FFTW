4cbcdef3fb3b722b0ee7d7e0a76b8bf5bca8fcdd
Orlando Chen <seagochen@hotmail.com>
2019-05-10 10:48:22 +0800

    fixed little bugs when trying to build .a under macos

--- build/macos_build.sh
+++ build/macos_build.sh
@@ -9,8 +9,9 @@ set -e
 mkdir -p $INSTALL_DIR
 rm -rf $INSTALL_DIR/*
 
-for arch in i386 x86_64; do
-    export CC="gcc -arch ${arch}"
+#for arch in i386 x86_64; do
+for arch in x86_64; do
+    # export CC="gcc -arch ${arch}"
 
     ./configure --enable-float
     make -j4
@@ -19,7 +20,7 @@ for arch in i386 x86_64; do
     make distclean
 done
 
-lipo -arch i386 $INSTALL_DIR/libfftw3f_i386.a -arch x86_64 $INSTALL_DIR/libfftw3f_x86_64.a -create -output $INSTALL_DIR/libfftw3f.a
+# lipo -arch i386 $INSTALL_DIR/libfftw3f_i386.a -arch x86_64 $INSTALL_DIR/libfftw3f_x86_64.a -create -output $INSTALL_DIR/libfftw3f.a
 
 # Copy the header file too, just for convenience
 cp api/fftw3.h $INSTALL_DIR/fftw3.h
